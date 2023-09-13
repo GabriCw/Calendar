022ea5a26fab18b22dd8a3676f148326850f63bd
Gabriel <gabricouto2305@gmail.com>
2023-09-13 18:52:01 -0300

    Removendo items

--- CalendarApp/view/calendar.js
+++ CalendarApp/view/calendar.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Agenda } from 'react-native-calendars';
+import { Button } from 'react-native-paper';
 
 class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items:{
+        '2023-09-13': [{id:1, name: 'Reunião de trabalho', time: '14:00 - 16:00'  }],
+        '2023-09-14': [{id:2, name: 'Ligar para o cliente', time: '11:00 - 11:30' }, {id:3, name: 'Ligar para o cliente', time: '11:00 - 11:30' }],
+      }
+    };
+  }
+
+  handleDelete = (itemId) => {
+      const itemsNew = this.state.items;
+      for(let i in this.state.items){
+        for(let j in this.state.items[i]){
+          if(this.state.items[i][j].id == itemId){
+            alert(itemsNew[i][j].name + ' foi deletado com sucesso!');
+            itemsNew[i].splice(j, 1);
+            this.setState({items: itemsNew });
+          }
+        }
+      }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 , paddingTop: 30 }}>
         <Agenda
-          items={{
-            '2023-09-13': [{ name: 'Reunião de trabalho' }],
-            '2023-09-14': [{ name: 'Ligar para o cliente' }, { name: 'Fazer compras' }],
-            '2023-09-15': [],
-          }}
+          items={this.state.items}
           renderItem={(item) => (
             <View style={styles.item}>
-              <Text>{item.name}</Text>
+              <View>
+                <Text style={styles.time}>{item.time}</Text>
+                <Text>{item.name}</Text>
+              </View>
+              <Button style={styles.btn} onPress={() => this.handleDelete(item.id)}><Text style={styles.btntxt}>X</Text></Button>
             </View>
           )}
         />
@@ -31,7 +55,27 @@ const styles = StyleSheet.create({
     padding: 10,
     marginRight: 10,
     marginTop: 17,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
+  time: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    paddingBottom: 10,
+  },
+  btn: {
+    width: 40,
+    height: 40, 
+    backgroundColor: '#f0f0f0',
+    borderRadius: '100%',
+    marginRight: 40,
+  },
+  btntxt: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#4F4F4F',
+  }
 });
 
 export default Calendar;
